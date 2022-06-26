5a30732963e66f903fd5f450f74b98a568c4b91e
kid7258 <kid7258@naver.com>
2022-06-26 22:56:29 +0900

    JPA 페이징과 정렬

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -66,11 +66,33 @@ class MemberJpaRepositoryTest {
         Member memberB = new Member("memberA", 20);
         memberJpaRepository.save(memberA);
         memberJpaRepository.save(memberB);
-        
+
         List<Member> findMembers = memberJpaRepository.findByUsernameAndAgeGreaterThan("memberA", 15);
 
         assertThat(findMembers.get(0).getUsername()).isEqualTo("memberA");
         assertThat(findMembers.get(0).getAge()).isEqualTo(20);
         assertThat(findMembers.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void paging() {
+        memberJpaRepository.save(new Member("member1", 10));
+        memberJpaRepository.save(new Member("member2", 10));
+        memberJpaRepository.save(new Member("member3", 10));
+        memberJpaRepository.save(new Member("member4", 10));
+        memberJpaRepository.save(new Member("member5", 10));
+
+        int age = 10;
+        int offset = 1;
+        int limit = 3;
+
+        List<Member> members = memberJpaRepository.findByPage(age, offset, limit);
+        for (Member member : members) {
+            System.out.println("member = " + member);
+        }
+        long totalCount = memberJpaRepository.totalCount(age);
+
+        assertThat(members.size()).isEqualTo(3);
+        assertThat(totalCount).isEqualTo(5);
     }
 }
